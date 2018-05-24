dd56574661e962e6a535a320c1dcf2642be7c162
Jefferson M Souza <jefferson.msouza86@gmail.com>
2018-05-23 22:20:44 -0300

    Creating new test scenarios

--- src/test/java/br/com/jmsstudio/automation/bid/BidSystemTest.java
+++ src/test/java/br/com/jmsstudio/automation/bid/BidSystemTest.java
@@ -10,6 +10,7 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class BidSystemTest {
@@ -28,7 +29,8 @@ public class BidSystemTest {
     public void shouldCreateANewUser() {
         WebDriver driver = EnvironmentManager.getDriver();
 
-        driver.get("http://localhost:8080/usuarios/new");
+        driver.get("http://localhost:8080/usuarios");
+        driver.findElement(By.linkText("Novo Usuário")).click();
 
         final String userName = "John Armless";
         final String userEmail = "john.armless@johnarmless.com";
@@ -41,11 +43,46 @@ public class BidSystemTest {
         emailField.sendKeys(userEmail);
         saveButton.click();
 
-        new WebDriverWait(driver, 5000).until(ExpectedConditions.textToBe(By.xpath("//*[@id=\"content\"]/h1"), "Todos os Usuários"));
+        new WebDriverWait(driver, 5).until(ExpectedConditions.textToBe(By.xpath("//*[@id=\"content\"]/h1"), "Todos os Usuários"));
 
         assertTrue(driver.getPageSource().contains(userName));
         assertTrue(driver.getPageSource().contains(userEmail));
     }
 
+    @Test
+    public void shouldShowErrorMessageWhenTryingToCreateAUserWithoutName() {
+        WebDriver driver = EnvironmentManager.getDriver();
+
+        driver.get("http://localhost:8080/usuarios/new");
+
+        final String userName = "";
+        final String userEmail = "john.armless@johnarmless.com";
+
+        final WebElement nameField = driver.findElement(By.name("usuario.nome"));
+        final WebElement emailField = driver.findElement(By.name("usuario.email"));
+        final WebElement saveButton = driver.findElement(By.id("btnSalvar"));
+
+        nameField.sendKeys(userName);
+        emailField.sendKeys(userEmail);
+        saveButton.click();
+
+        assertEquals("Nome obrigatorio!", driver.findElement(By.xpath("//*[@id=\"content\"]")).getText().split("\n")[0]);
+
+    }
+
+    @Test
+    public void shouldShowErrorMessageWhenTryingToCreateAUserWithoutNameAndEmail() {
+        WebDriver driver = EnvironmentManager.getDriver();
+
+        driver.get("http://localhost:8080/usuarios/new");
+
+        final WebElement saveButton = driver.findElement(By.id("btnSalvar"));
+
+        saveButton.click();
+
+        assertEquals("Nome obrigatorio!", driver.findElement(By.xpath("//*[@id=\"content\"]")).getText().split("\n")[0]);
+        assertEquals("E-mail obrigatorio!", driver.findElement(By.xpath("//*[@id=\"content\"]")).getText().split("\n")[1]);
+
+    }
 
 }
